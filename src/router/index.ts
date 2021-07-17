d7d40f75c65aee6177b8024109ffcabee5a03a48
ReinoldsKora <reinoldskora@ReinoldsKoras-MacBook-Pro.local>
2021-07-17 18:59:08 +0300

    add auth route guard, move extract access token logic to auth guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Main from '@/views/Main.vue'
+import store from '@/store'
+import { getAccesTokenFromHash } from '@/composable/auth'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,17 +13,54 @@ const routes: Array<RouteRecordRaw> = [
     path: '/config.html',
     name: 'Config',
     component: () => import('../views/Config.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/panel.html',
     name: 'Panel',
     component: () => import('../views/Panel.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+
+  console.log('call')
+  if (!store.getters['auth/token']) {
+
+    const hashInfo = getAccesTokenFromHash()
+
+    if (hashInfo) {
+      store.commit('auth/SET_AUTH_DATA', hashInfo)
+      localStorage.setItem('authInfo', JSON.stringify(hashInfo))
+      // push to the same route for remove hash params
+      next({ name: to.name || 'Config' })
+      return
+    }
+
+  }
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+
+    if (!store.getters['auth/token']) {
+
+      next({ name: 'Main' }); return
+
+    }
+
+  }
+
+  next()
+  
 })
 
 export default router
--- src/views/Config.vue
+++ src/views/Config.vue
@@ -6,28 +6,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useStore } from 'vuex'
-import { getAccesTokenFromHash } from '@/composable/auth'
-import { useRouter, useRoute } from 'vue-router'
 
 export default defineComponent({
   name: 'About',
-  setup() {
-
-    const store = useStore()
-    const router = useRouter()
-    const route = useRoute()
- 
-    const hashInfo = getAccesTokenFromHash()
-
-    if (hashInfo) {
-      store.commit('auth/SET_AUTH_DATA', hashInfo)
-      localStorage.setItem('authInfo', JSON.stringify(hashInfo))
-      // push to the same route for remove hash params
-      router.push({ name: route.name || 'Config' })
-    }
-
-  },
 });
 </script>
 
