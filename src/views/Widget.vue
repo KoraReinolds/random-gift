3636790f4486b4e0df06ab94d4c85729ea86856c
KoraReinolds <s.shurin94@gmail.com>
2021-10-10 03:20:30 +0300

    load model to scene

--- src/views/Widget.vue
+++ src/views/Widget.vue
@@ -13,6 +13,7 @@ import {
   onMounted,
 } from 'vue'
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default defineComponent({
   name: 'Widget',
@@ -33,23 +34,36 @@ export default defineComponent({
 			renderer.setSize( window.innerWidth, window.innerHeight )
 			widget.value.appendChild( renderer.domElement )
 
-			const geometry = new THREE.BoxGeometry()
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-			const cube = new THREE.Mesh( geometry, material )
-			scene.add( cube )
 
-			camera.position.z = 2
+      const light = new THREE.AmbientLight(0xFFFFFF, 5)
+			scene.add( light )
+
+      const directionalLight = new THREE.DirectionalLight( 0xffffff, 3.5 )
+      scene.add( directionalLight )
+
+			camera.position.z = 10
 
 			const animate = function () {
 				requestAnimationFrame( animate )
-
-				cube.rotation.x += 0.01
-				cube.rotation.y += 0.01
-
 				renderer.render( scene, camera )
 			}
 
 			animate()
+
+      const loader = new GLTFLoader()
+
+      loader.load(
+        'knife.glb',
+        gltf => {
+          scene.add( gltf.scene )
+        },
+        xhr => {
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' )
+        },
+        error => {
+          console.log( error, 'An error happened' )
+        }
+      );
 
     }
 
