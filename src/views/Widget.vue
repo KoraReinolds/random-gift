8233c988da55bcf4a64333f1ceb14804e89871ed
KoraReinolds <s.shurin94@gmail.com>
2021-10-19 00:14:17 +0300

    lighting scene

--- src/views/Widget.vue
+++ src/views/Widget.vue
@@ -14,6 +14,8 @@ import {
 } from 'vue'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js'
 
 export default defineComponent({
   name: 'Widget',
@@ -27,43 +29,69 @@ export default defineComponent({
 
     const initScene = () => {
 
+      let mixer: THREE.AnimationMixer
+      let exrCubeRenderTarget: any
+      const clock = new THREE.Clock()
 			const scene = new THREE.Scene()
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
+      // init camera
+			const camera = new THREE.PerspectiveCamera(
+        75, window.innerWidth / window.innerHeight, 0.1, 1000
+      )
+      camera.position.z = 10
+
+      // init renderer
 			const renderer = new THREE.WebGLRenderer({ alpha: true })
-			renderer.setSize( window.innerWidth, window.innerHeight )
-			widget.value.appendChild( renderer.domElement )
+      renderer.outputEncoding = THREE.sRGBEncoding
+      renderer.physicallyCorrectLights = true
+			renderer.setSize(window.innerWidth, window.innerHeight)
+			widget.value.appendChild(renderer.domElement)
 
+      // Orbit controls
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.update()
 
-      const light = new THREE.AmbientLight(0xFFFFFF, 5)
-			scene.add( light )
+      // init exr texture
+      const pmremGenerator = new THREE.PMREMGenerator(renderer)
+      pmremGenerator.compileEquirectangularShader()
+      new EXRLoader().load('snowy_cemetery_4k.exr', texture => {
+        exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture)
+        texture.dispose()
+      })
 
-      const directionalLight = new THREE.DirectionalLight( 0xffffff, 3.5 )
-      scene.add( directionalLight )
+      // init model
+      const loader = new GLTFLoader()
+      loader.load(
+        'watch.glb',
+        gltf => {
+          scene.add(gltf.scene)
+          mixer = new THREE.AnimationMixer(gltf.scene)
+          gltf.animations.forEach(clip => {
+            mixer.clipAction( clip ).play()
+          })
+        },
+        xhr => {
+          console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+        },
+        error => {
+          console.log(error, 'An error happened')
+        }
+      )
 
-			camera.position.z = 10
-
-			const animate = function () {
-				requestAnimationFrame( animate )
-				renderer.render( scene, camera )
+			const animate = () => {
+				requestAnimationFrame(animate)
+        const delta = clock.getDelta()
+        scene.traverse(child => {
+          if ( !(child instanceof THREE.Mesh) ) return
+          child.material.envMapIntensity = 2.0
+          child.material.envMap = exrCubeRenderTarget?.texture
+          child.material.needsUpdate = true
+        })
+        mixer?.update(delta)
+				renderer.render(scene, camera)
 			}
 
 			animate()
-
-      const loader = new GLTFLoader()
-
-      loader.load(
-        'knife.glb',
-        gltf => {
-          scene.add( gltf.scene )
-        },
-        xhr => {
-          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' )
-        },
-        error => {
-          console.log( error, 'An error happened' )
-        }
-      );
 
     }
 
