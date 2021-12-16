c7e50ff4b8c72a858df3a48b853876ae39945fec
KoraReinolds <s.shurin94@gmail.com>
2021-12-17 01:20:59 +0300

    add base layout for choose product

---
+++ src/components/ProductApperance.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="flex-row-center-center w-100p"
+  >
+    <div
+      :class="['arrow prev disabled']"
+      @click="!disabled && nextProduct"
+    />
+
+    <div
+      class="relative img"
+    >
+      <img
+        class="absolute top-left w-100p h-100p"
+        :src="`type${productItem + 1}.gif`"
+      />
+    </div>
+
+    <div
+      :class="['arrow next disabled']"
+      @click="!disabled && nextProduct"
+    />
+
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'ProductApperance',
+  setup() {
+    const productItem = ref(1)
+
+    const nextProduct = () => {
+      productItem.value = +(!productItem.value)
+    }
+
+    return {
+      productItem,
+      nextProduct,
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.img {
+  width: 300px;
+  height: 300px;
+}
+
+.arrow {
+  border: 24px solid var(--main-color);
+  border-top: 24px solid transparent;
+  border-right: 24px solid transparent;
+  cursor: pointer;
+
+  &.disabled {
+    cursor: default;
+    border-bottom-color: var(--disabled-color);
+    border-left-color: var(--disabled-color);
+  }
+
+  &.next {
+    transform: rotate(225deg);
+  }
+
+  &.prev {
+    transform: rotate(45deg);
+  }
+}
+
+
+
+</style>
--- src/views/Config.vue
+++ src/views/Config.vue
@@ -7,6 +7,9 @@
       v-if="!step"
       @changeStep="changeStep"
     />
+    <product-apperance
+      v-else-if="step === 1"
+    />
     <config-product
       v-else-if="step === 2"
       v-for="item in config.giftList"
@@ -18,12 +21,12 @@
     >
       <base-button
         v-if="step"
-        class="mr-24"
         @click="changeStep(0)"
         v-text="'Back'"
       />
       <base-button
         v-if="!step"
+        class="ml-24"
         @click="changeStep(1)"
         v-text="'Continue'"
       />
@@ -37,11 +40,13 @@ import { useStore } from 'vuex'
 import ConfigProduct from '@/components/ConfigProduct.vue'
 import ConfigMain from '@/components/ConfigMain.vue'
 import BaseButton from '@/components/BaseButton.vue'
+import ProductApperance from '@/components/ProductApperance.vue'
 import { useConfiguration } from '@/composable/configuration'
 
 export default defineComponent({
   name: 'About',
   components: {
+    ProductApperance,
     ConfigProduct,
     ConfigMain,
     BaseButton,
@@ -49,7 +54,7 @@ export default defineComponent({
   setup() {
     
     const { config, saveConfig } = useConfiguration()
-    const step = ref(0)
+    const step = ref(1)
     const changeStep = (newStep: number) => step.value = newStep
 
     const store = useStore()
