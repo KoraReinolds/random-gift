d4419a4a26bebce0fa68863ac5c081c7256929d6
KoraReinolds <s.shurin94@gmail.com>
2022-02-03 02:43:31 +0300

    Revert "split config steps to separate routes"
    This reverts commit 498bc78b022b56fbfa712bb7857a1eb8d23e1ee7.

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -11,19 +11,45 @@
       :finishedSteps="item.finishedSteps"
       @changeStep="changeStep"
     />
-    <router-view />
+    <config-main
+      v-if="step === '0'"
+      @changeStep="changeStep"
+    />
+    <product-apperance
+      v-else-if="step === '1'"
+      :item="item"
+      @changeStep="changeStep"
+    />
+    <config-product
+      v-else-if="step === '2'"
+      :item="item"
+      @changeStep="changeStep"
+    />
+    <product-cost
+      v-else-if="step === '3'"
+      :item="item"
+      @changeStep="changeStep"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { useStore } from 'vuex'
+import ConfigProduct from '@/components/ConfigProduct.vue'
+import ConfigMain from '@/components/ConfigMain.vue'
 import AddProductHeader from '@/components/AddProduct.vue'
+import ProductApperance from '@/components/ProductApperance.vue'
+import ProductCost from '@/components/ProductCost.vue'
 import { useConfiguration } from '@/composable/configuration'
 
 export default defineComponent({
   name: 'About',
   components: {
+    ProductCost,
+    ProductApperance,
+    ConfigProduct,
+    ConfigMain,
     AddProductHeader,
   },
   setup() {
