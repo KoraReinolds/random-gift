654c8f04e144304f6df6c9f580137dac030a85e0
KoraReinolds <s.shurin94@gmail.com>
2022-02-23 15:00:05 +0300

    header new layout

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="add-product flex-row-center-center w-100p"
+    class="add-product flex-row-center-center w-100p my-16"
   >
     <div
-      v-for="index in ['1', '2', '3']"
+      v-for="index in ['<', '1', '2', '3', '>']"
       :key="`section-${index}`"
       :class="[
-        `add-product__section pointer pa-32 h-100p flex-row-center-center`,
+        `add-product__section pointer h-100p w-100p flex-row-center-center relative flex-row`,
         { active: index === step },
         { finished: item.finishedSteps.includes(index) },
         { disabled: !item.availableSteps.includes(index) },
@@ -14,23 +14,30 @@
       @click="item.availableSteps.includes(index) && changeStep(index)"
     >
       <div
-        class="border mr-8 flex-row-center-center fs-24 bold w-48 h-48 rounded"
+        :class="'grow h-16 bg-epic line first'"
+      />
+      <div
+        class="add-product__cell border flex-row-center-center fs-24 bold w-48 h-48 rounded"
       >
         <icon
           v-if="item.finishedSteps.includes(index)"
           name="accept"
-          :width="32"
-          :height="32"
         />
         <div
           v-else
           v-text="index"
+          :width="32"
+          :height="32"
         />
       </div>
       <div
-        class="fs-24 my-16 bold"
-        v-text="$t(`configMain.${index - 1}.title`)"
+        :class="'grow h-16 bg-epic line last'"
       />
+      <!-- <div
+        v-if="index === step"
+        class="fs-24 ml-8 mr-16 bold"
+        v-text="$t(`configMain.${index - 1}.title`)"
+      /> -->
     </div>
   </div>
 </template>
@@ -56,31 +63,42 @@ export default defineComponent({
 <style scoped lang="scss">
 
 .add-product {
-  max-width: 900px;
+  max-width: $config-body-max-width;
+
+//   &__cell {
+//     position: relative;
+//     top: 50px;
+//   }
 
   &__section {
-    max-width: 300px;
-
-    &.disabled {
-      cursor: default;
-
-      div {
-        color: var(--disabled-color);
-      }
+    &:last-child .line.last,
+    &:first-child .line.first {
+      visibility: hidden;
     }
+//     position: relative;
+//     max-width: 300px;
 
-    &.finished div {
-      color: var(--common-color);
-    }
+//     &.disabled {
+//       cursor: default;
 
-    &.active div {
-      color: var(--light-main-color);
-    }
-    
-    
-    &:not(.add-product__section:last-child) {
-      border-right: 2px solid var(--disabled-color);
-    }
+//       div {
+//         color: var(--disabled-color);
+//       }
+//     }
+
+//     &.finished div {
+//       color: var(--common-color);
+//     }
+
+//     &.active {
+//       div {
+//         color: var(--light-main-color);
+//       }
+//     }
+
+//     &.add-product__section {
+//       border-bottom: 2px solid var(--disabled-color);
+//     }
 
   }
 }
