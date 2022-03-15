fbf3b5f500e098315f184eeb1eefe0099692f801
KoraReinolds <s.shurin94@gmail.com>
2022-03-16 00:28:32 +0400

    script setup on Config

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -4,68 +4,38 @@
     v-if="config"
   >
     <span v-text="config" class="c-font"/>
-    <add-product-header
-      v-if="step !== '0' && item"
+    <AddProductHeader
+      v-if="configStep !== '0' && item"
     />
-    <config-main
-      v-if="step === '0'"
+    <ConfigMain
+      v-if="configStep === '0'"
     />
-    <product-apperance
-      v-else-if="step === '1'"
+    <ProductApperance
+      v-else-if="configStep === '1'"
       :item="item"
     />
-    <config-product
-      v-else-if="step === '2'"
+    <ConfigProduct
+      v-else-if="configStep === '2'"
       :item="item"
     />
-    <product-cost
-      v-else-if="step === '3'"
+    <ProductCost
+      v-else-if="configStep === '3'"
       :item="item"
     />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useStore } from 'vuex'
-import ConfigProduct from '@/components/ConfigProduct.vue'
-import ConfigMain from '@/components/ConfigMain.vue'
-import AddProductHeader from '@/components/AddProduct.vue'
-import ProductApperance from '@/components/ProductApperance.vue'
-import ProductCost from '@/components/ProductCost.vue'
-import { useConfiguration } from '@/composable/configuration'
-
-export default defineComponent({
-  name: 'About',
-  components: {
-    ProductCost,
-    ProductApperance,
-    ConfigProduct,
-    ConfigMain,
-    AddProductHeader,
-  },
-  setup() {
+<script setup lang="ts">
+  import { useStore } from 'vuex'
+  import ConfigProduct from '@/components/ConfigProduct.vue'
+  import ConfigMain from '@/components/ConfigMain.vue'
+  import AddProductHeader from '@/components/AddProduct.vue'
+  import ProductApperance from '@/components/ProductApperance.vue'
+  import ProductCost from '@/components/ProductCost.vue'
+  import { useConfiguration } from '@/composable/configuration'
     
-    const { config, item, configStep, changeStep } = useConfiguration()
-    const store = useStore()
+  const { config, item, configStep } = useConfiguration()
 
-    const pushNewGift = () => {
-      store.commit('config/ADD_GIFT_TO_LIST', {
-        title: '123',
-        chanceType: ['common'],
-      })
-    }
-
-    return {
-      item,
-      step: configStep,
-      changeStep,
-      pushNewGift,
-      config,
-    }
-
-  }
-});
 </script>
 
 <style lang="scss" scoped>
