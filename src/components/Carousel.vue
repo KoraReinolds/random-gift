79a889131927a9ced9a7244d50e30f3dd7e816ac
KoraReinolds <s.shurin94@gmail.com>
2022-03-15 23:52:21 +0400

    script setup on Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -7,7 +7,7 @@
       :src="`type${index + 1}.gif`"
     >
     <ul
-      :class="'dots py-8 absolute w-100p flex-row-center-center'"
+      :class="['dots py-8 absolute w-100p flex-row-center-center']"
     >
       <li
         v-for="(imgSrc, i) in list"
@@ -15,7 +15,7 @@
           { active: index === i }
         ]"
         :key="imgSrc"
-        @click="changeIndex(i + 1)"
+        @click="changeIndex(+i + 1)"
       >
         <div
           :class="['rounded h-100p w-100p bg-font']"
@@ -25,37 +25,25 @@
   </div>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
+<script setup lang="ts">
 
-export default defineComponent({
-  name: 'Carousel',
-  props: {
-    list: {
-      type: Array,
-      require: true,
-    }
-  },
-  setup(props) {
+import { ref, defineProps } from 'vue'
 
-    const index = ref(0)
-    let interval
-    const changeIndex = (newIndex) => {
-      if (interval) clearInterval(interval)
+  const props = defineProps<{
+    list: string[]
+  }>()
+  const index = ref(0)
+  let interval: number
+  const changeIndex = (newIndex: number) => {
+    if (interval) clearInterval(interval)
+    index.value = newIndex % props.list.length
+    interval = setInterval(() => {
+      newIndex += 1;
       index.value = newIndex % props.list.length
-      interval = setInterval(() => {
-        newIndex += 1;
-        index.value = newIndex % props.list.length
-      }, 5000)
-    }
-    changeIndex(index.value)
-
-    return {
-      index,
-      changeIndex,
-    }
+    }, 5000)
   }
-});
+  changeIndex(index.value)
+
 </script>
 
 <style scoped lang="scss">
