5d5349e779aaa90330f8d6927e671ab9b6db98f8
KoraReinolds <s.shurin94@gmail.com>
2022-03-16 12:44:11 +0400

    script setup on Icon

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -12,58 +12,35 @@
   />
 </template>
 
-<script>
-import { defineComponent, onMounted, ref, computed } from 'vue'
+<script setup lang="ts">
+  import { defineProps, onMounted, ref, computed } from 'vue'
 
-export default defineComponent({
-  name: 'Icon',
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    width: {
-      type: Number,
-      default: undefined,
-    },
-    height: {
-      type: Number,
-      default: undefined,
-    },
-    filled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props) {
+  const props = defineProps<{
+    name: string
+    width?: number
+    height?: number
+    filled?: boolean
+  }>()
 
-    const originalWidth = ref('')
-    const originalHeight = ref('')
-    const svgHTML = computed(() => require(`@/assets/svg/${props.name}.svg`))
-    const icon = ref(null)
+  const originalWidth = ref('')
+  const originalHeight = ref('')
+  const svgHTML = computed(() => require(`@/assets/svg/${props.name}.svg`))
+  const icon = ref()
 
-    onMounted(() => {
-      const el = icon.value.firstChild;
+  onMounted(() => {
+    const el = icon.value.firstChild;
 
-      if (!el) return
-  
-      originalWidth.value = el.getAttribute('width');
-      originalHeight.value = el.getAttribute('height');
+    if (!el) return
 
-      el.style.maxHeight = props.height
-      el.style.maxWidth = props.width
-  
-      el.removeAttribute('height')
-      el.removeAttribute('width')
-    })
-    return {
-      originalWidth,
-      originalHeight,
-      svgHTML,
-      icon,
-    }
-  }
-});
+    originalWidth.value = el.getAttribute('width');
+    originalHeight.value = el.getAttribute('height');
+
+    el.style.maxHeight = props.height
+    el.style.maxWidth = props.width
+
+    el.removeAttribute('height')
+    el.removeAttribute('width')
+  })
 </script>
 
 <style lang="scss">
