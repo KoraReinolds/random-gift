1f4b8ecea98ca645bcd075f11004b322741e15e7
KoraReinolds <s.shurin94@gmail.com>
2022-03-19 21:43:47 +0300

    script setup on ConfigProduct

--- src/components/ConfigProduct.vue
+++ src/components/ConfigProduct.vue
@@ -2,44 +2,43 @@
   <div
     class="config-product flex-row"
   >
-
     <div
       class="chances flex-column-start-center"
     >
       <h1
         class="my-8"
-        v-text="$t(`title.configItem.${steps[step].title}`)"
+        v-text="$t(`title.configItem.${steps[index].title}`)"
       />
 
       <div class="chances__content flex-row-center-center relative">
 
-        <fragment-shader
+        <FragmentShader
           class="absolute top-left w-100p h-100p absolute"
-          :color="steps[step].title"
+          :color="steps[index].title"
         />
 
-        <chances-values
+        <ChancesValues
           :chances="item.chances"
           @change="changeIndex"
-          :step="step"
+          :step="index"
           :disabled="!configValid"
         /> 
         
-        <action-list
+        <ActionList
           class="chances__actions h-100p"
           :list="actionList"
-          :isValid="validationActionList[step]"
+          :isValid="validationActionList[index]"
           :chance="chanceValue"
-          :empty="step == 0"
+          :empty="index == 0"
         />
 
-        <chance-bar
-          :disabled="step === '0'"
+        <ChanceBar
+          :disabled="index === '0'"
           :modelValue="chanceValue"
           :maxValue="maxValue"
           @update:modelValue="recalculateChances({
             chances: item.chances,
-            type: steps[step].title,
+            type: steps[index].title,
             value: $event,
           })"
         />
@@ -57,127 +56,92 @@
     </div>
 
   </div>
-    <div
-      class="flex-row-center-center mt-48"
-    >
-      <base-button
-        @click="changeStep('1')"
-        v-text="$t('btn.configBack')"
-      />
-      <base-button
-        class="ml-8"
-        @click="changeIndex(+step + 1)"
-        v-text="$t('btn.configContinue')"
-        :disabled="!configValid"
-      />
-    </div>
+  <div
+    class="flex-row-center-center mt-48"
+  >
+    <BaseButton
+      @click="changeStep('1')"
+      v-text="$t('btn.configBack')"
+    />
+    <BaseButton
+      class="ml-8"
+      @click="changeIndex(+index + 1)"
+      v-text="$t('btn.configContinue')"
+      :disabled="!configValid"
+    />
+  </div>
 </template>
 
-<script lang="ts">
-import BaseButton from '@/components/BaseButton.vue'
-import FragmentShader from '@/components/FragmentShader.vue'
-import ChancesValues from '@/components/ChancesValues.vue'
-import ActionList from '@/components/ActionList.vue'
-import ChanceBar from '@/components/ChanceBar.vue'
-import { defineComponent, PropType, ref, computed } from 'vue'
-import { Gift, ChangeChances } from '@/store/config/types'
-import { useProducts } from '@/composable/products'
-import { useConfiguration } from '@/composable/configuration/index'
-import { useStore } from 'vuex'
+<script setup lang="ts">
+  import BaseButton from '@/components/BaseButton.vue'
+  import FragmentShader from '@/components/FragmentShader.vue'
+  import ChancesValues from '@/components/ChancesValues.vue'
+  import ActionList from '@/components/ActionList.vue'
+  import ChanceBar from '@/components/ChanceBar.vue'
+  import { ref, computed, defineProps } from 'vue'
+  import { Gift, ChangeChances } from '@/store/config/types'
+  import { useConfiguration } from '@/composable/configuration/index'
+  import { useStore } from 'vuex'
 
-export default defineComponent({
-  name: 'ConfigProduct',
-  components: {
-    BaseButton,
-    FragmentShader,
-    ChancesValues,
-    ActionList,
-    ChanceBar,
-  },
-  props: {
-    item: {
-      type: Object as PropType<Gift>,
-      required: true
-    }
-  },
-  setup(props) {
+  const props = defineProps<{
+    item: Gift
+  }>()
 
-    const { changeStep, saveConfig, changeFinishedSteps, changeAvailableSteps } = useConfiguration()
-    const nextStep = '3'
-    const { bitsCost } = useProducts()
-    const store = useStore()
-    const index = ref('1')
-    const changeIndex = (newIndex: number) => {
-      if (newIndex === steps.length) {
-        changeAvailableSteps(nextStep)
-        changeFinishedSteps('2')
-        changeStep(nextStep)
-        saveConfig()
-      } else {
-        index.value = `${newIndex % steps.length}`
-      }
-    }
-    const recalculateChances = (params: ChangeChances) => {
-      store.commit('config/CHANGE_ITEM_CHANCES', params)
-    }
-    const steps = [
-      { title: 'none' },
-      { title: 'common' },
-      { title: 'rare' },
-      { title: 'epic' },
-      { title: 'legendary' },
-    ]
-    const chancesList = computed(() => {
-      return Object.values(props.item.chances)
-    })
-
-    const chanceValue = computed(() => {
-      return `${chancesList.value[+index.value]}`
-    })
-
-    const maxValue = computed(() => {
-      const [none, ...rest] = Object.values(props.item.chances)
-      const noneValue = 100 - rest.reduce((sum, cur) => sum + +cur, 0)
-      return index.value === "0"
-        ? `${noneValue}`
-        : `${noneValue + +chanceValue.value}`
-    })
-
-    const actionList = computed(() => {
-      return Object.values(props.item.actions)[+index.value]
-    })
-
-    const validationActionList = computed(() => {
-      return Object.values(props.item.actions).map((list) => {
-        if (!list.length) return true
-        return list.every(itemList => !!itemList.value)
-      })
-    })
-
-    const chanceIsFull = computed(() => {
-      return chancesList.value.reduce((sum, cur) => sum + +cur, 0) === 100
-    })
-
-    const configValid = computed(() => {
-      return validationActionList.value.every(isValid => isValid)
-    })
-
-    return {
-      changeStep,
-      chanceIsFull,
-      configValid,
-      step: index,
-      steps,
-      changeIndex,
-      bitsCost,
-      recalculateChances,
-      actionList,
-      chanceValue,
-      maxValue,
-      validationActionList,
+  const { changeStep, saveConfig, changeFinishedSteps, changeAvailableSteps } = useConfiguration()
+  const nextStep = '3'
+  const store = useStore()
+  const index = ref('1')
+  const changeIndex = (newIndex: number) => {
+    if (newIndex === steps.length) {
+      changeAvailableSteps(nextStep)
+      changeFinishedSteps('2')
+      changeStep(nextStep)
+      saveConfig()
+    } else {
+      index.value = `${newIndex % steps.length}`
     }
   }
-});
+  const recalculateChances = (params: ChangeChances) => {
+    store.commit('config/CHANGE_ITEM_CHANCES', params)
+  }
+  const steps = [
+    { title: 'none' },
+    { title: 'common' },
+    { title: 'rare' },
+    { title: 'epic' },
+    { title: 'legendary' },
+  ]
+  const chancesList = computed(() => {
+    return Object.values(props.item.chances)
+  })
+
+  const chanceValue = computed(() => {
+    return `${chancesList.value[+index.value]}`
+  })
+
+  const maxValue = computed(() => {
+    const [none, ...rest] = Object.values(props.item.chances)
+    const noneValue = 100 - rest.reduce((sum, cur) => sum + +cur, 0)
+    return index.value === "0"
+      ? `${noneValue}`
+      : `${noneValue + +chanceValue.value}`
+  })
+
+  const actionList = computed(() => {
+    return Object.values(props.item.actions)[+index.value]
+  })
+
+  const validationActionList = computed(() => {
+    return Object.values(props.item.actions).map((list) => {
+      if (!list.length) return true
+      return list.every(itemList => !!itemList.value)
+    })
+  })
+
+  const configValid = computed(() => {
+    return validationActionList.value.every(isValid => isValid)
+  })
+
 </script>
 
 <style scoped lang="scss">
